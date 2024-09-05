8aad288ebf79231ebfa93b5addbd13045865de5b
Brian Vieira <brian.vieira@ebankit.com>
2024-09-05 16:16:46 +0100

    removed code

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const dbName = 'loginDB';
 let db;
 
 // Conectar ao MongoDB
-MongoClient.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+MongoClient.connect(mongoURI)
     .then(client => {
         db = client.db(dbName);
         console.log("Successfully connected to the database");
